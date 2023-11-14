c1348c9a55424dd5e96ab757b5fd66ba097a9635
habibmalik <habibmalik.professional@gmail.com>
2023-11-14 12:21:25 +0700

    add unit test for Test Result Features

---
+++ Test/Features/TestResultFeatures/Data/GetTotalPassingApplicantsData/SucceedData.cs
@@ -0,0 +1,161 @@
+﻿using Domain.Entities;
+using Domain.Enums;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Features.TestResultFeatures.Data.GetTotalPassingApplicantsData
+{
+    public class SucceedData : TheoryData<List<ApplicantTestResult>, int>
+    {
+        public SucceedData()
+        {
+            // Data 1 
+            // Passed both total score and division score criteria
+            var data1 = new List<ApplicantTestResult>();
+            data1.Add(new ApplicantTestResult
+            {
+                ApplicantType = ApplicantDivision.Humanities,
+                TestScores = new TestScores
+                {
+                    English = 100,
+                }
+            });
+
+            // Failed both score criteria
+            data1.Add(new ApplicantTestResult
+            {
+                ApplicantType = ApplicantDivision.Humanities,
+                TestScores = new TestScores
+                {
+                    English = 0,
+                    Math = 0,
+                    Science = 0,
+                    Japanese = 0,
+                    Geography = 0
+                }
+            });
+
+            Add(data1, 1);
+
+            // Data 2
+            // Passed only total score criteria
+            var data2 = new List<ApplicantTestResult>();
+            data2.Add(new ApplicantTestResult
+            {
+                ApplicantType = ApplicantDivision.Humanities,
+                TestScores = new TestScores
+                {
+                    English = 95
+                }
+            });
+
+            // Passed both total score and division score criteria
+            data2.Add(new ApplicantTestResult
+            {
+                ApplicantType = ApplicantDivision.Humanities,
+                TestScores = new TestScores
+                {
+                    English = 100
+                }
+            });
+
+            Add(data2, 1);
+
+            // Data 3
+            // Passed only division score criteria
+            var data3 = new List<ApplicantTestResult>();
+            data3.Add(new ApplicantTestResult
+            {
+                ApplicantType = ApplicantDivision.Humanities,
+                TestScores = new TestScores
+                {
+                    English = 90
+                }
+            });
+
+            // Passed both total score and division score criteria
+            data3.Add(new ApplicantTestResult
+            {
+                ApplicantType = ApplicantDivision.Humanities,
+                TestScores = new TestScores
+                {
+                    English = 100
+                }
+            });
+
+            Add(data3, 1);
+
+            // Data 4
+            // Passed both score criteria
+            var data4 = new List<ApplicantTestResult>();
+            data4.Add(new ApplicantTestResult
+            {
+                ApplicantType = ApplicantDivision.Humanities,
+                TestScores = new TestScores
+                {
+                    English = 100
+                }
+            });
+
+            // Passed both score criteria
+            data4.Add(new ApplicantTestResult
+            {
+                ApplicantType = ApplicantDivision.Humanities,
+                TestScores = new TestScores
+                {
+                    English = 100
+                }
+            });
+
+            // Passed only division criteria
+            data4.Add(new ApplicantTestResult
+            {
+                ApplicantType = ApplicantDivision.Humanities,
+                TestScores = new TestScores
+                {
+                    English = 90
+                }
+            });
+
+            Add(data4, 2);
+
+            // Data 5
+            // Passed both score criteria
+            var data5 = new List<ApplicantTestResult>();
+            data5.Add(new ApplicantTestResult
+            {
+                ApplicantType = ApplicantDivision.Humanities,
+                TestScores = new TestScores
+                {
+                    English = 100
+                }
+            });
+
+            // Passed both score criteria
+            data5.Add(new ApplicantTestResult
+            {
+                ApplicantType = ApplicantDivision.Humanities,
+                TestScores = new TestScores
+                {
+                    English = 100
+                }
+            });
+
+            // Passed both score criteria
+            data5.Add(new ApplicantTestResult
+            {
+                ApplicantType = ApplicantDivision.Humanities,
+                TestScores = new TestScores
+                {
+                    English = 100
+                }
+            });
+
+            Add(data5, 3);
+        }
+    }
+}
---
+++ Test/Features/TestResultFeatures/GetTotalPassingApplicants/GetTotalPassingApplicantsTests.cs
@@ -0,0 +1,47 @@
+﻿using Application.Common.Interfaces;
+using Application.Features.TestResultFeatures.Queries.GetTotalPassingApplicants;
+using Domain.Entities;
+using Domain.Enums;
+using NSubstitute;
+using NSubstitute.Extensions;
+using Test.Features.TestResultFeatures.Data.GetTotalPassingApplicantsData;
+
+namespace Test.Features.TestResultFeatures.GetTotalPassingApplicants
+{
+    public class GetTotalPassingApplicantsTests
+    {
+        [Theory]
+        [ClassData(typeof(SucceedData))]
+        public async Task ShouldSucceedGetTotalPassingApplicants(List<ApplicantTestResult> testResults, int expectedPassingApplicants)
+        {
+            // Arrange
+            var inputParserServiceMock = Substitute.For<IInputParserService>();
+            var testValidationServiceMock = Substitute.For<ITestValidationService>();
+
+            // Mock Service
+            inputParserServiceMock.ParseInputToTestResult(Arg.Any<string>()).Returns(testResults);
+
+            // Simulate individual validations with English score -> when English score is 100, the applicant passed both validations.
+            // When English score is 95, the applicant only passed total score validation, but not division score validation.
+            // When English score is 90, the applicant only passed division score validation.
+            testValidationServiceMock.ValidateTotalScore(Arg.Any<TestScores>()).Returns(false);
+            testValidationServiceMock.ValidateTotalScore(Arg.Is<TestScores>(x => x.English == 100)).Returns(true);
+            testValidationServiceMock.ValidateTotalScore(Arg.Is<TestScores>(x => x.English == 95)).Returns(true);
+
+            testValidationServiceMock.ValidateDivisionScore(Arg.Any<ApplicantDivision>(), Arg.Any<TestScores>()).Returns(false);
+            testValidationServiceMock.ValidateDivisionScore(Arg.Any<ApplicantDivision>(), Arg.Is<TestScores>(x => x.English == 100)).Returns(true);
+            testValidationServiceMock.ValidateDivisionScore(Arg.Any<ApplicantDivision>(), Arg.Is<TestScores>(x => x.English == 90)).Returns(true);
+
+
+            var handler = new GetTotalPassingApplicantsHandler(testValidationServiceMock, inputParserServiceMock);
+            var query = new GetTotalPassingApplicantsQuery("Test String");
+
+            // Act
+            var actualResult = await handler.Handle(query, default);
+
+            // Assert
+            Assert.Equal(expectedPassingApplicants, actualResult);
+        }
+        
+    }
+}
